0354f89f32ae17c13decbc844cd7288f21d874b9
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2018-09-11 10:29:54 -0400

    reset bob-is-cool for demo purposes

--- src/examples/bob-is-cool.component.js
+++ src/examples/bob-is-cool.component.js
@@ -6,64 +6,9 @@ p {
 
 const template = `
 <style>${styles}</style>
-<p><span id="name"></span> is cool!!!</p>
+<p>Bob is cool!!!</p>
 `;
 
-class BobIsCool extends HTMLElement {
-	constructor() {
-		super();
-
-		this._$name = undefined;
-		this._name = '';
-
-		this._changeHanlders = {};
-		this._changeHanlders.name = (oldValue, newValue) => this.handleNameChange(oldValue, newValue);
-	}
-
-	handleNameChange(oldValue, newValue) {
-		if (oldValue === newValue) {
-			return;
-		}
-
-		this.name = newValue;
-	}
-
-	connectedCallback() {
-		this.innerHTML = template;
-
-		this._$name = this.querySelector('#name');
-		this.name = this.getAttribute('name') || 'Bob';
-	}
-
-	get name() {
-		return this._name;
-	}
-
-	set name(newName) {
-		if (newName === this.name) {
-			return;
-		}
-
-		this._$name.textContent = newName;
-		this.setAttribute('name', newName);
-		this._name = newName;
-	}
-
-	static get observedAttributes() {
-		return ['name'];
-	}
-
-	attributeChangedCallback(attrName, oldValue, newValue) {
-		let handler = this._changeHanlders[attrName];
-
-		if (handler) {
-			handler(oldValue, newValue);
-		}
-	}
-
-	disconnectedCallback() {
-		console.log('You haven\'t seen the last of me ...');
-	}
-}
+class BobIsCool extends HTMLElement {}
 
 window.customElements.define('bob-is-cool', BobIsCool);
